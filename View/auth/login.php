2b656450f368dbaab1f169902bf95027f909f3b6
kobe <youcode@skiff.com>
2023-12-29 12:14:45 +0100

    added the login page

--- View/auth/login.php
+++ View/auth/login.php
@@ -8,98 +8,53 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
 $errors = $_SESSION['errors'] ?? [];
 session_unset();
 $pageTitle = 'Login';
+include '../templates/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estatein - Discover a place you'll love to live</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link href="Assets/css/style.css" rel="stylesheet">
+<div class="flex items-center justify-center h-screen">
+    <div class="bg-white p-8 rounded shadow-md w-full max-w-sm">
+        <h2 class="text-2xl font-bold mb-4 text-gray-800">Login</h2>
 
-</head>
-
-<body class="custom-bg ">
-    <?php include '../../View/templates/header.php'; ?>
-    <!-- Hero section -->
-    <div class="centred custom-bg rounded shadow-lg p-6 mt-12 md:flex md:items-center relative">
-        <!-- Hero image with centered overlay text -->
-        <div class="md:w-1/2 relative">
-            <img src="Assets/img/hiDpiExtraLarge.webp" alt="Hero Image" class="w-full h-auto">
-            <div class="absolute inset-0 flex items-center justify-center">
-                <div class="text-center">
-                    <h1 class="text-white text-4xl font-bold">Discover a place<br> you'll love to live</h1>
+        <form action="../../app/Controller/LoginController.php" method="POST">
+            <?php if (isset($errors['login'])): ?>
+                <div class="mb-4 text-red-500">
+                    <?php echo $errors['login']; ?>
                 </div>
+            <?php endif; ?>
+            <!-- Email Field -->
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    Email
+                </label>
+                <input class="shadow appearance-none border <?php echo isset($errors['email']) ? 'border-red-500' : ''; ?> rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" name="email" type="text" placeholder="Email">
+                <?php if (isset($errors['email'])): ?>
+                    <span class="text-red-500 text-xs italic"><?php echo $errors['email']; ?></span>
+                <?php endif; ?>
             </div>
-        </div>
 
-        <div class="md:w-1/2 mt-6 md:mt-0 md:ml-6">
-            <div class="form-inputs-container">
-                <div class="mb-4 text-center">
-                    <h2 class="text-2xl font-bold text-white mb-4">Login</h2>
-
-                    <form action="../../app/Controller/LoginController.php" method="POST">
-                        <?php if (isset($errors['login'])): ?>
-                            <div class="mb-4 text-red-500">
-                                <?php echo $errors['login']; ?>
-                            </div>
-                        <?php endif; ?>
-
-                        <!-- Email Field -->
-                        <div class="mb-4">
-                            <label for="email" class="customLabel">Email</label>
-                            <input id="email"
-                                class="p-2 border <?php echo isset($errors['email']) ? 'border-red-500' : 'border-gray-300'; ?> rounded w-full"
-                                type="email" placeholder="Email" />
-                            <?php if (isset($errors['email'])): ?>
-                                <span
-                                    class="text-<?php echo isset($errors['email']) ? 'red-500' : 'gray-300'; ?> text-xs italic">
-                                    <?php echo $errors['email']; ?>
-                                </span>
-                            <?php endif; ?>
-                        </div>
-
-
-                        <!-- Password Field -->
-                        <div class="mb-6">
-                            <label class="customLabel" for="password">Password</label>
-                            <input id="password"
-                                class="p-2 border <?php echo isset($errors['password']) ? 'border-red-500' : 'border-gray-300'; ?> rounded w-full"
-                                type="password" placeholder="Password" />
-                            <?php if (isset($errors['password'])): ?>
-                                <span
-                                    class="text-<?php echo isset($errors['password']) ? 'red-500' : 'gray-300'; ?> text-xs italic">
-                                    <?php echo $errors['password']; ?>
-                                </span>
-                            <?php endif; ?>
-                        </div>
-
-
-                        <!-- Submit Button -->
-                        <div class="flex items-center justify-between">
-                            <button
-                                class="sign-up-button bg-purple-600 text-white px-6 py-2 rounded transition duration-300 hover:bg-purple-700 focus:outline-none focus:ring"
-                                type="submit">
-                                Sign In
-                            </button>
-                            <p class="text-white ml-4"><a class="text-purple-300 hover:underline"
-                                    href="/path-to-forgot-password">
-                                    Forgot Password?
-                                </a></p>
-                        </div>
-                    </form>
-                </div>
+            <!-- Password Field -->
+            <div class="mb-6">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input class="shadow appearance-none border <?php echo isset($errors['password']) ? 'border-red-500' : ''; ?> rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="********">
+                <?php if (isset($errors['password'])): ?>
+                    <span class="text-red-500 text-xs italic"><?php echo $errors['password']; ?></span>
+                <?php endif; ?>
             </div>
-        </div>
+
+            <!-- Submit Button -->
+            <div class="flex items-center justify-between">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                    Sign In
+                </button>
+                <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/path-to-forgot-password">
+                    Forgot Password?
+                </a>
+            </div>
+        </form>
     </div>
+</div>
 
-
-        <?php include '../../View/templates/footer.php'; ?>
-
-        <script src="Assets/js/myscript.js"></script>
 </body>
-
 </html>
